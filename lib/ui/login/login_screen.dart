6a201072f21a8411fbdc80ae61e7cf4fb9504b5b
Rzldo <ezas4531@gmail.com>
2023-05-11 00:48:22 +0700

    create login ui

--- lib/ui/login/login_screen.dart
+++ lib/ui/login/login_screen.dart
@@ -1,6 +1,13 @@
+import 'package:biodiv/utils/colors.dart';
+import 'package:biodiv/utils/custom_button.dart';
+import 'package:biodiv/utils/custom_textfield.dart';
+import 'package:biodiv/utils/validation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+
+import '../../utils/text_style.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,11 +17,66 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  final _usernameController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColor.backgroundColor,
+      resizeToAvoidBottomInset: false,
       body: Container(
-        padding: const EdgeInsets.all(24.0),
+        padding: const EdgeInsets.all(35.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              "asset/image/LogoBiodiversity.png",
+              width: MediaQuery.of(context).size.width * 0.45,
+            ),
+            const SizedBox(
+              height: 35,
+            ),
+            Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    CustomTextField(
+                        hintText: "username",
+                        controller: _usernameController,
+                        validator: Validator.basicValidate,
+                        obsecure: false),
+                    const SizedBox(
+                      height: 30,
+                    ),
+                    CustomTextField(
+                        hintText: "password",
+                        controller: _passwordController,
+                        validator: Validator.validatePassword,
+                        obsecure: true)
+                  ],
+                )),
+            const SizedBox(
+              height: 30,
+            ),
+            CustomButton(text: "Login", onTap: () {}),
+            const SizedBox(
+              height: 30,
+            ),
+            RichText(
+              text: TextSpan(children: <TextSpan>[
+                const TextSpan(
+                    text: "Not Have Account ? ",
+                    style: ReusableTextStyle.basic),
+                TextSpan(
+                    text: "Register!",
+                    style: ReusableTextStyle.basicMainColorBold,
+                    recognizer: TapGestureRecognizer()..onTap = () {})
+              ]),
+            )
+          ],
+        ),
       ),
     );
   }
